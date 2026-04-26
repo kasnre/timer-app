00980d7d58e9f8a4edcdd94f5a14821618c82d87
kasnre <kasnre@gmail.com>
2026-04-26 14:01:49 +0800

    Delete src directory

--- src/db.js
+++
@@ -1,6 +0,0 @@
-import Dexie from 'dexie';
-
-export const db = new Dexie('TimerAppDB');
-db.version(2).stores({ // 升級版本號到 2 以套用新結構
-  tasks: 'id, title, order_rank, duration, is_running, start_time, mode, target_time'
-});
